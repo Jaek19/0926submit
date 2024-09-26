18415e249266610659f3822db624bc1d2629fdde
Jaek19 <promise20203@naver.com>
2024-09-26 10:15:34 +0900

    Add files via upload

---
+++ 반려동물/contents.js
@@ -0,0 +1,36 @@
+document.getElementById('certification-popup').addEventListener('click', () => {
+  document.getElementById('certification-wrapper').style.display = 'block';
+});
+
+function deleteCertification() {
+  document.getElementById('certification-wrapper').style.display = 'none';
+}
+
+const breederBtn = document.getElementById('help-breeder');
+
+breederBtn.addEventListener('click', () => {
+  alert('작업중입니다.');
+});
+
+const myPageBtn = document.getElementById('mypageBtn');
+const bellBtn = document.getElementById('bellBtn');
+const myPageText = document.getElementById('myPageText');
+
+function initialAppear() {
+  bellBtn.style.visibility = 'hidden';
+  myPageText.style.visibility = 'hidden';
+}
+
+initialAppear();
+function myPageAppear() {
+  if (
+    bellBtn.style.visibility === 'hidden' &&
+    myPageText.style.visibility === 'hidden'
+  ) {
+    bellBtn.style.visibility = 'visible';
+    myPageText.style.visibility = 'visible';
+  } else {
+    bellBtn.style.visibility = 'hidden';
+    myPageText.style.visibility = 'hidden';
+  }
+}
